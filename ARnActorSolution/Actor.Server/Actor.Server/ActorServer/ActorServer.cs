40598e95ef0a86ccb470561db5831d6b37892719
ARn <arnaud.bernheim@outlook.fr>
2015-10-21 11:04:25 +0200

    ba

---
+++ ARnActorSolution/Actor.Server/Actor.Server/ActorServer/ActorServer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actor.Base
+{
+    public class ActorServer : actActor
+    {
+        public string Name { get; private set; }
+        public int Port { get; private set; }
+        private string fFullHost = "" ;
+        public string FullHost { get 
+        {
+            if (string.IsNullOrEmpty(fFullHost))
+            {
+                fFullHost = Fullhost();
+            }
+            return fFullHost;
+        } }
+
+        private string Fullhost()
+        {
+            var localhost = Dns.GetHostName();
+            var prefix = "http://";
+            var suffix = ":" + Port.ToString();
+            var fullhost = prefix + localhost + suffix + "/" + Name + "/";
+            return fullhost;
+        }
+
+        private static ActorServer fServerInstance = null ;
+
+        public static ActorServer GetInstance()
+        {
+            return fServerInstance ;
+        }
+
+        public static void Start(string lName, int lPort, bool withRelay = true)
+        {
+            fServerInstance = new ActorServer(lName,lPort) ;
+            fServerInstance.DoInit(withRelay);
+        }
+
+        public ActorServer(string lName, int lPort)
+        {
+            Name = lName;
+            Port = lPort;
+            actTagHelper.SetFullHost(Fullhost());
+        }
+
+        private void DoInit(bool withRelay) 
+        {
+            actDirectory.GetDirectory(); // Start directory
+            new ActorConsole(); // Start console
+            // should work now
+            actSendByName<string>.SendByName("Actor Server Start", "Console");
+            Become(null);
+            if (withRelay)
+            {
+                new actShardDirectory(); // start shard directory
+                new actHostRelay();
+            }
+            // new actTcpServer();
+        }
+    }
+} 
