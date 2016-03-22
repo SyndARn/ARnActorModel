0af325cfa397cc9f487aaf68281c9c59a39de517
arn <arn>
2016-03-22 12:30:39 +0100

    Cleaning Configuration X64

---
+++ ARnActorSolution/Actor.TestApplication/WpfPrime/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Actor.Base;
+using Actor.Server;
+using Actor.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfPrime.Prime;
+
+namespace WpfPrime
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ActorServer.Start("localhost", 80, false);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var prime = new actPrime(100);
+            IActor act = new actActor();
+            prime.SendMessage(new Tuple<int, IActor>(100, act));
+        }
+    }
+}
---
+++ ARnActorSolution/Actor.TestApplication/WpfPrime/Prime/actPrime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Actor.Base;
+using Actor.Util;
+using Actor.Server;
+
+namespace WpfPrime.Prime
+{
+    public class actPrime : actActor
+    {
+        int prime;
+        public actPrime(int n)
+        {
+            prime = n;
+            Become(new bhvBehavior<Tuple<int, IActor>>(DoPrime));
+            AddBehavior(new bhvBehavior<int>(FindPrime));
+        }
+
+
+        private void FindPrime(int msg)
+        {
+            Console.WriteLine("Find diviser %0 of %1", msg, prime);
+        }
+
+        private void DoPrime(Tuple<int, IActor> msg)
+        {
+            if (msg.Item1 > 1)
+            {
+                if (msg.Item1 % prime == 0)
+                {
+                    msg.Item2.SendMessage(prime);
+                }
+                var actn1 = new actPrime(msg.Item1 -1);
+                for (int i = msg.Item1 - 1; i > 0; i--)
+                {
+                    actn1.SendMessage(new Tuple<int,IActor>(i, this));
+                }
+            }
+        }
+    }
+}
