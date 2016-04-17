8acb06ea15d88d3d670a5841eac535ca257d66e8
arn <arn>
2016-04-17 18:13:43 +0200

    Hello World

--- ARnActorSolution/Actor.Base/ActorBase/ActorExtension.cs
+++ ARnActorSolution/Actor.Base/ActorBase/ActorExtension.cs
@@ -65,4 +65,5 @@ namespace Actor.Base
             });
         }
     }
+
 }
---
+++ ARnActorSolution/Application/ARnActorHelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Actor.Base;
+
+namespace ARnActorHelloWorld
+{
+    class Program
+    {
+
+        class HelloWorld : BaseActor
+        {
+            public HelloWorld() : base()
+            {
+                Become(new Behavior<string>(t => Console.WriteLine(t))) ;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var act = new HelloWorld();
+            act.SendMessage("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
