3dc396e84b1375c9fc19465a7d8e1436301a65c4
arn <arn>
2016-05-17 00:20:21 +0200

    CheckArg test and MapReduce Test

---
+++ ARnActorSolution/TestActor/Exception/CheckArgTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Actor.Base;
+using System.IO;
+
+namespace TestActor.ExceptionTest
+{
+    [TestClass]
+    public class CheckArgTest
+    {
+
+        [TestMethod]
+        public void CheckArgReturnTest()
+        {
+            using (var stream = new MemoryStream())
+            {
+                CheckArg.Stream(stream);
+            }
+
+            try
+            {
+                CheckArg.Stream(null);
+                Assert.Fail();
+            }
+            catch(ActorException)
+            {
+            }
+
+            CheckArg.Address("My address");
+            try
+            {
+                CheckArg.Address(string.Empty);
+                Assert.Fail();
+            }
+            catch(ActorException)
+            {
+
+            }
+
+            CheckArg.Behaviors(new Actor.Base.Behaviors());
+            try
+            {
+                CheckArg.Behaviors(null);
+                Assert.Fail();
+            }
+            catch(ActorException)
+            {
+
+            }
+
+            CheckArg.Behavior(new Actor.Base.Behavior<string>());
+            try
+            {
+                CheckArg.Behavior(null);
+                Assert.Fail();
+            }
+            catch(ActorException)
+            {
+
+            }
+
+            CheckArg.Actor(new Actor.Base.BaseActor());
+            try
+            {
+                CheckArg.Actor(null);
+                Assert.Fail();
+            }
+            catch (ActorException)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ ARnActorSolution/TestActor/MapReduce/MapReduceTesting.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Actor.Util;
+using Actor.Base;
+using System.Linq;
+
+namespace TestActor.MapReduce
+{
+
+    class MapReduceSimpleTest
+    {
+        public MapReduceSimpleTest()
+        {
+
+        }
+
+        public void Go(string aFilename, IActor actor)
+        {
+            var mapReduce = new MapReduceActor<string, string, string, string, int>
+                (
+                // parse
+                (a, d) =>
+                {
+                    for (int i = 0; i < 10; i++)
+                        a.SendMessage(d,i.ToString()+" "+i.ToString());
+                },
+                // map
+                (a, k, v) =>
+                {
+                    string[] stab = v.Split(' ');
+                    foreach (var item in stab)
+                    {
+                        a.SendMessage(new Tuple<string, int>(item, 1));
+                    }
+
+                },
+                // reduce
+                (k, v) =>
+                {
+                    int sum = v.Count();
+                    return sum;
+                },
+                // output
+                actor);
+            mapReduce.SendMessage(aFilename);
+        }
+    }
+
+    [TestClass]
+    [Ignore]
+    public class MapReduceTesting
+    {
+
+
+
+        [TestMethod]
+        public void TestingSimpleMapReduce()
+        {
+            TestLauncherActor.Test(() =>
+            {
+                MapReduceSimpleTest mapReduce = new MapReduceSimpleTest();
+                EnumerableActor<string> actor = new EnumerableActor<string>();
+                mapReduce.Go("test map", actor);
+                
+                var result = actor.Count;
+                Assert.IsTrue(result > 0);
+            });
+        }
+    }
+}
