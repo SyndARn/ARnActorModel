0c7ced33f4e5edd6de41a693a90b6ac5ecbf428c
arn <arn>
2016-05-17 22:24:22 +0200

    Fixing

--- ARnActorSolution/TestActor/TextWriterActorTest.cs
+++ ARnActorSolution/TestActor/TextWriterActorTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Actor.Base;
 using Actor.Util;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace TestActor
 {
@@ -20,6 +21,7 @@ namespace TestActor
                     textWriter.SendMessage("1st line");
                     textWriter.SendMessage("2nd line");
                     textWriter.SendMessage("3rd line");
+                    Task.Delay(3000); // let some times to process ...
                 });
                 memoryStream.Seek(0, SeekOrigin.Begin);
                 using (StreamReader reader = new StreamReader(memoryStream))
