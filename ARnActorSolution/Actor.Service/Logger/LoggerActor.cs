1201d0195490e6934639456265de9671c16906e8
ARn <arnbernheim@orange.fr>
2018-03-22 00:35:08 +0100

    CoreCleanDir

---
+++ ARnActorSolution/Actor.Service/Logger/LoggerActor.cs
@@ -0,0 +1,16 @@
+﻿using Actor.Server;
+using Actor.Base;
+
+namespace Actor.Service
+{
+    public class LoggerActor : BaseActor
+    {
+        public LoggerActor(string aFileName) : base()
+        {
+            Become(new LoggerBehaviors(aFileName));
+            var heartBeat = new HeartBeatActor(500);
+            heartBeat.SendMessage(this);
+            SendMessage("Logging start");
+        }
+    }
+}
---
+++ ARnActorSolution/src/shared/Actor.Util.Shared/Collection/DistributedDictionarySetBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Actor.Base;
+
+namespace Actor.Util
+{
+    public class DistributedDictionarySetBehavior<TKey, TValue> : Behavior<TKey, TValue>
+    {
+        private DistributedDictionaryBehaviors<TKey, TValue> Parent()
+        {
+            return this.LinkedTo as DistributedDictionaryBehaviors<TKey, TValue>;
+        }
+        public DistributedDictionarySetBehavior() : base()
+        {
+            Pattern = (k, v) => true;
+            Apply = (k, v) =>
+            {
+                var hashActor = Parent().GetHashActor(k);
+                hashActor.SendMessage(k, v);
+            };
+        }
+    }
+}
