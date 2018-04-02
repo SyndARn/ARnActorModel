598bae18dada37bfdcdf3ac9ef670cef2f830d6f
SyndARn <arnbernheim@orange.fr>
2018-04-02 20:54:08 +0200

    Separating Producer and Consumer

---
+++ ARnActorSolution/src/shared/Actor.Util.Shared/ProducerConsumer/Consumer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Actor.Base;
+
+namespace Actor.Util
+{
+    public class Consumer<T> : FsmActor<string, Work<T>>
+    {
+        public Consumer() : base()
+        {
+            var bhv = new FsmBehaviors<string, Work<T>>();
+
+            bhv
+                .AddRule("SleepState", null, t => t.SendMessage(this), "BusyState")
+                .AddRule("BusyState", null, t => Buffer.SendMessage(this), "SleepState")
+                .AddBehavior(new Behavior<Work<T>>(
+                    t =>
+                    {
+                        this.SendMessage(GetCurrentState().Result(), t);
+                    }));
+
+            Become(bhv);
+        }
+
+        public Buffer<T> Buffer { get; set; }
+    }
+}
