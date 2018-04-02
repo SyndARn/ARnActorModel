3b54d354b6f5624e8f05f1603c27792580954bb4
SyndARn <arnbernheim@orange.fr>
2018-04-02 15:06:44 +0200

    Moving Actor.Server Directory

---
+++ ARnActorSolution/src/Actor.Server/Properties/ARnActorModelVersion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("ARn SyndARn")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.4.5.*")]
