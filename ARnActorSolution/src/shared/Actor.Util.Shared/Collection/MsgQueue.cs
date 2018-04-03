08a8e3ecd9f3db28416965d3153e345373aaa5d8
arn <arn>
2018-04-03 05:58:49 +0200

    Core go

---
+++ ARnActorSolution/src/shared/Actor.Util.Shared/Collection/MsgQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actor.Util
+{
+    public interface IMsgQueue<T>
+    {
+        bool Result { get; }
+        T Data { get; }
+    }
+
+    public class MsgQueue<T> : IMsgQueue<T>
+    {
+        public bool Result { get; }
+        public T Data { get; }
+        public MsgQueue(bool aResult, T aData)
+        {
+            Result = aResult;
+            Data = aData;
+        }
+    }
+}
