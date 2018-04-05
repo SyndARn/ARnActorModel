46e9c9234369e372d899fbb7ccf6febfb40f0a87
SyndARn <arnbernheim@orange.fr>
2018-04-05 22:54:01 +0200

    Brace Refacto

---
+++ ARnActorSolution/src/shared/Actor.Util.Shared/BehaviorDecorated/BehaviorAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actor.Util
+{
+#if !(NETFX_CORE) 
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class BehaviorAttribute : Attribute
+    {
+    }
+#endif
+}
