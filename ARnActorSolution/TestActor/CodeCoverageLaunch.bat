83e738fc01a4a7a34b9c8123e155c37260b6046b
ARn <SyndARn@users.noreply.github.com>
2020-04-11 12:28:30 +0200

    Update CodeCoverageLaunch.bat

--- ARnActorSolution/TestActor/CodeCoverageLaunch.bat
+++ ARnActorSolution/TestActor/CodeCoverageLaunch.bat
@@ -1,10 +1,10 @@
 ﻿@Echo Testing
-cd "C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\packages\OpenCover.4.6.519\tools"
+cd "C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\packages\OpenCover.4.7.922\tools"
 echo "opencover.console.exe" "-register:user" -target:"C:\Program Files (x86)\Microsoft visual Studio 14.0\Common7\IDE\mstest.exe" -output:"C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\TestActor\coverage.xml" -targetargs:"/testcontainer:C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\TestActor\bin\Debug\TestActor.dll" -filter:+[*]* -mergebyhash
 "opencover.console.exe" "-register:user" -target:"C:\Program Files (x86)\Microsoft visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" -output:"C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\TestActor\coverage.xml" -targetargs:"C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\TestActor\bin\Debug\TestActor.dll" -filter:+[*]* -mergebyhash
 cd "C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\TestActor\"
 @Echo Reporting
-cd "C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\packages\ReportGenerator.2.4.5.0\tools"
+cd "C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\packages\ReportGenerator.4.5.4\tools"
 "ReportGenerator.exe" "-reports:C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\TestActor\coverage.xml" "-targetdir:C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\TestActor\Report" -Reporttypes:Badges;Html;HtmlSummary
 cd "C:\Users\ARn\Source\Repos\ARnActorModel\ARnActorSolution\TestActor\"
 pause
